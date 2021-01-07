7acc160aa529b61b8bda38ae01474d8c706a1ae4
Birendra Negi <birendra.beeru@gmail.com>
2021-01-07 13:30:22 -0500

    Program to find the sum of defth of each node in a binary tree

---
+++ ProgAndAlgo/com/java/tree/SumOfTreeNodeDefth.java
@@ -0,0 +1,39 @@
+package com.java.tree;
+
+
+/** Algo Expert- Program return sum of defth of each node **/
+public class SumOfTreeNodeDefth {
+
+	  public static int nodeDepths(BinaryTree root) {
+	    // Write your code here.
+
+			if(root == null)
+			{
+				return 0;
+			}
+	    return nodeDefth(root,0);
+	  }
+		public static int nodeDefth(BinaryTree root, int defth) {
+	    // Write your code here.
+			
+			if(root.left == null && root.right == null)
+			{
+				return defth;
+			}
+			
+			return defth + (root.left != null ? nodeDefth(root.left, defth+1) : 0) + (root.right != null ? nodeDefth(root.right , defth +1): 0);
+
+	  }
+
+	  static class BinaryTree {
+	    int value;
+	    BinaryTree left;
+	    BinaryTree right;
+
+	    public BinaryTree(int value) {
+	      this.value = value;
+	      left = null;
+	      right = null;
+	    }
+	  }
+}
