711c410dff8375c24d5d8e37c4e33f6a3c2d9011
Birendra Negi <birendra.beeru@gmail.com>
2021-01-10 12:42:54 -0500

    for a given target find 2 elements in given array who's sum is equal to target

---
+++ ProgAndAlgo/com/java/array/FindTargetSumFor2Element.java
@@ -0,0 +1,26 @@
+package com.java.array;
+/** for a given target find 2 elements in given array who's sum is equal to target **/
+public class FindTargetSumFor2Element {
+	 public static int[] twoNumberSum(int[] array, int targetSum) {
+		    // Write your code here.
+				int [] ansPair = new int[0];
+				int next;
+				for(int i=0; i < array.length; i++)
+				{
+					next = targetSum - array[i];
+							for(int j = i+1; j < array.length; j++)
+					{
+						if(array[j] == next)
+						{
+							ansPair = new int[2];
+							ansPair[0] = array[i];
+							ansPair[1] = next;
+							break;
+						}
+					}
+					
+				}
+		    return ansPair;
+		  }
+
+}
